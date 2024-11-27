53796d3ddbacac916e09276f333a3b82db7fa73b
yurigmaia <yuri.g.maia96@gmail.com>
2024-11-27 20:25:07 -0300

    removed unused files

--- clinioapplication/src/main/java/com/example/backend/repositories/ConsultaRepository.java
+++ clinioapplication/src/main/java/com/example/backend/repositories/ConsultaRepository.java
@@ -1,13 +1,13 @@
 package com.example.backend.repositories;
 
-import com.example.backend.models.ConsultaModel;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Optional;
-import java.util.UUID;
+import com.example.backend.models.ConsultaModel;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<ConsultaModel, UUID> {
--- cliniofrontend/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
